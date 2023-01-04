8aca5f72044e1db21e455c2dca7021b26ebc5028
arif1018 <51040618+arif1018@users.noreply.github.com>
2023-01-04 17:07:50 +0500

    Delete server directory

--- server/backend/routes/userRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const { addUser } = require("../controllers/userController")
-const router = express.Router()
-
-router.post("/adduser/",addUser)
-
-module.exports = router
